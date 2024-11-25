b0d42d7f32ece067af7c9ed3c3a6ca674327f4b9
Kitae's Github <aqua0405@gmail.com>
2024-11-25 11:52:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from todo.todo import todo_router
 
 app = FastAPI()
 
@@ -6,3 +7,5 @@ app = FastAPI()
 @app.get("/")
 async def welcome() -> dict:
     return {"message": "Welcome to FastAPI!"}
+
+app.include_router(todo_router) # todo_router를 app에 포함시킴 
